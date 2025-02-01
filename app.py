962ebe365b88fbcb417f7c9cc1303b799f7b926e
bhattaraijyoti <bhattataijyoti2115@gmail.com>
2025-02-01 14:05:38 +0545

    Implement pagination and update navigation links

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 import json
 import os
+import math
 from datetime import datetime
 
 
@@ -14,6 +15,7 @@ with open('config.json', 'r') as c:
   
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
+
 
 app.config['UPLOAD_FOLDER']=params['upload_location']
 
@@ -59,9 +61,28 @@ class Post(db.Model):
 
 # Routes
 @app.route("/")
-def home():
-    post=Post.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params , post=post)
+def home(): 
+    post=Post.query.filter_by().all()
+    last=math.ceil(len(post)/int(params['no_of_posts']))
+     #[0:params['no_of_posts']]
+    page=request.args.get('page')
+    if(not str(page).isnumeric()):
+        page=1
+    page=int(page)
+    post=post[(page-1)*int(params['no_of_posts']) : (page-1)*int(params['no_of_posts']) + int(params['no_of_posts'])]
+    if (page==1):
+        prev="#"
+        next="/?page="+ str(page+1)     
+    elif(page==last):
+        prev="/?page="+ str(page-1)  
+        next="#" 
+    else:
+        prev="/?page="+ str(page-1)  
+        next="/?page="+ str(page+1)
+
+
+   
+    return render_template('index.html', params=params , post=post , prev=prev , next=next )
 
 
 
